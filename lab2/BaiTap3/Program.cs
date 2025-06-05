b30bce91a228c1c2904af071a4fa8f26ace6c419
HoangAnh268 <hoanganh2005163@gmail.com>
2025-06-05 13:38:34 +0700

    .

---
+++ lab2/BaiTap3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks;
+
+namespace BaiTap3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Queue<string> khachhang = new Queue<string>();
+            khachhang.Enqueue("Hoàng Anh");
+            khachhang.Enqueue("Đăng Khoa");
+            khachhang.Enqueue("Thanh Thảo");
+            khachhang.Enqueue("Tấn Dũng");
+            khachhang.Enqueue("Hồng Phúc");
+
+            
+            Console.WriteLine("Hàng đợi kết thúc: " + khachhang.Dequeue());
+            Console.WriteLine("Hàng đợi kết thúc: " + khachhang.Dequeue());
+
+            Console.WriteLine("Khách hàng còn lại:");
+            foreach (var ten in khachhang) 
+                Console.WriteLine(ten);
+            Console.ReadLine();
+        }
+    }
+}

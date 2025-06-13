a18fc81ebccad0abf0b5f13a15e76f4af7bddda4
HoangAnh268 <hoanganh2005163@gmail.com>
2025-06-13 11:00:00 +0700

    .

---
+++ lab5/BaiTap2TL/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+namespace BaiTap2TL
+{
+    public delegate void NumberAddEventHandler(int Number);
+   public class NumberList
+    {
+        private List<int> numbers = new List<int>();
+        public event NumberAddEventHandler NumberAdd;
+        public void AddNumber(int number)
+        {
+            numbers.Add(number);
+            NumberAdd?.Invoke(number);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberList list = new NumberList();
+            list.NumberAdd += OnNumberAdd;
+            list.AddNumber(10);
+            list.AddNumber(20);
+            list.AddNumber(30);
+            list.AddNumber(40);
+            list.AddNumber(50);
+        }
+        public static void OnNumberAdd(int number)
+        {
+            Console.WriteLine($"Số vừa thêm vào listnumbers: {number}");
+        }
+
+    }
+}

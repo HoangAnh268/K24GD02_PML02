aa7eb91b98867f1706bc0d8d7d38e205c823e6cc
HoangAnh268 <hoanganh2005163@gmail.com>
2025-06-13 11:03:05 +0700

    .

---
+++ lab5/BaiTap3TL/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+namespace BaiTap3TL
+{
+    internal class Program
+    {
+        delegate int Operation(int x, int y);
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Operation sum = delegate (int x, int y)
+            {
+                return x + y;
+            };
+            int Tong = sum(5, 7);
+            Console.WriteLine("Tổng của 5 và 7 là: " + Tong);
+        }
+        
+    }
+}
